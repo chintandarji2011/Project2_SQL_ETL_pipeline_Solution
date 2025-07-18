84374a3826780a16ae900e8ccc5d267657221161
chintandarji2011 <chintandarji2011@gmail.com>
2025-07-18 09:59:16 +0530

    Delete 06_export_final.sql

--- 06_export_final.sql
+++
@@ -1,6 +0,0 @@
--- 6.Export final tables and documentation.
--- Export final tables and documentation
-
-COPY employees TO 'D:\ETL_sql_project_file\Final_employees.csv' DELIMITER ',' CSV HEADER;
-
-COPY etl_audit_log TO 'D:\ETL_sql_project_file\Final_etl_audit_log.csv' DELIMITER ',' CSV HEADER;
